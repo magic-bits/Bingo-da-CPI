88978d29627c2b46fe57c281452d03e3c07b7d68
magicbits_ <73851455+magic-bits@users.noreply.github.com>
2021-05-28 01:23:01 -0300

    Códigos do Bingo da CPI

---
+++ feijao.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class feijao : MonoBehaviour
+{
+    [SerializeField] private GameObject _imagem;
+    [SerializeField] private static int contador = 25;
+    
+    public void spawn_faijao()
+    {
+        if (contador > 0)
+        {
+            Instantiate(_imagem, new Vector3(0, 0, 0), Quaternion.identity);
+            contador--;
+        }
+    }
+}
